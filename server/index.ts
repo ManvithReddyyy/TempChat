29ed41a0b89b767ab9cb6405be6c3cdea1a60503
ManvithReddyyy <23B81A05LV@cvr.ac.in>
2025-11-13 10:40:49 +0530

    Fix Render build

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,5 @@
+//process.env.NODE_ENV = "production";
+
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
